3fdcd0869e3f8e8e19268d5d8329c0d2a3dde0d6
shiwasmi <sagarchattar@gmail.com>
2026-06-04 20:34:52 +0530

    Implemanted new code

---
+++ src/main/java/com/example/bookmyplan_practice/WinterCamp.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan_practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WinterCamp {
+
+    @GetMapping("/WinterCamp")
+    public String getName() {
+        return "Welcome to WinterCamp";
+    }
+}
