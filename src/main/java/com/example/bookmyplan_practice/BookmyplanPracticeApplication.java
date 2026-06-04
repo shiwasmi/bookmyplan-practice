b97eaaefb1953ef70b8ade25756fc2029d087809
shiwasmi <sagarchattar@gmail.com>
2026-06-04 20:02:21 +0530

    Repository push

---
+++ src/main/java/com/example/bookmyplan_practice/BookmyplanPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyplanPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyplanPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookmyplan_practice/BookmyplanPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyplanPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
